ad60ad50b10f19a6a7e0d16b66765de7d660a376
Jeremie Alexander <122463783+JeremieAlex@users.noreply.github.com>
2023-01-11 12:37:24 -0500

    Update volumeEstimato.R

--- volumeEstimato.R
+++ volumeEstimato.R
@@ -6,11 +6,17 @@ Data = read.csv("~/Documents/BIOL_432/BIOL432_Assignment1/measurements.csv")
 
 library("dplyr")
 
-#Create new measurement file with volume column 
+#Create new measurement file with volume column. Use volume of cylinder equation pi*r*h^2 (r = Limb_Width/2, h = Limb_Length)
 
-measurements = Data %>% mutate(volume=(Limb_Length*Limb_Width))
+measurements = Data %>% mutate(volume=((pi)*(Limb_Width/2)*(Limb_Length^2)))
+
+#Check new data frame to ensure it is properly formatted
+
+head(measurements)
+tail(measurements)
 
 
 #Create new measurement.csv file that will overwrite the original
 
 write.csv(measurements, "~/Documents/BIOL_432/BIOL432_Assignment1/measurements.csv", row.names = TRUE)
+
